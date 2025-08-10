e5640a931f67e615a6035d36283e2b35b6b3cbf0
Ricardo Perello <ricardo.perellomas@epfl.ch>
2025-08-10 19:09:43 +0200

    part one: push to stack completed

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -6,7 +6,7 @@ pub struct EvmResult {
 }
 
 pub fn evm(_code: impl AsRef<[u8]>) -> EvmResult {
-    let stack: Vec<U256> = Vec::new();
+    let mut stack: Vec<U256> = Vec::new();
     let mut pc = 0;
 
     let code = _code.as_ref();
@@ -15,15 +15,44 @@ pub fn evm(_code: impl AsRef<[u8]>) -> EvmResult {
         let opcode = code[pc];
         pc += 1;
 
-        if opcode == 0x00 {
-            // STOP
+        match opcode {
+            0x00 => {
+                // STOP
+            }
+            0x5f => {
+                // PUSH0
+                stack.push(U256::from(0));
+            }
+            0x60..=0x7f => {
+                // PUSH1..PUSH32
+                let size = (opcode - 0x60) + 1;
+                let size = size as usize; // Cast to usize
+
+                
+                // Read 'size' bytes from the code
+                let mut value = U256::from(0);
+                for i in 0..size {
+                    if pc + i < code.len() {
+                        value = value << 8 | U256::from(code[pc + i]);
+                    }
+                }
+                
+                stack.push(value);
+                pc += size; // Skip over the bytes we just read
+            }
+            _ => {
+                return EvmResult {
+                    stack: stack,
+                    success: false,
+                };
+            }
         }
     }
 
     // TODO: Implement me
 
     return EvmResult {
-        stack: stack,
+        stack: stack.into_iter().rev().collect(),
         success: true,
     };
 }
